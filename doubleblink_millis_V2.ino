b35b3cb8f1c64fbc607c8cde366c80c4aaaadb24
Antonio Gordillo <antonio.gordillo.guerrero@gmail.com>
2017-01-02 14:54:38 +0100

    Add files via upload

---
+++ doubleblink_millis_V2.ino
@@ -0,0 +1,73 @@
+///////////////////////////////////////////////////////////////////////////////////
+//   Double Blink
+//   Blinks on two LEDs for two diferent periods using the millis function.
+//
+// Based on:
+//    - NodeMCU and Arduino open hardware and software community. Thank you all!
+//
+// Licensed under Creative Commons (SH-BY-NC). @AGordiGuerrero
+//
+// Antonio Gordillo Guerrero
+// anto@unex.es
+// @AGordiGuerrero
+// Smart Open Lab. Escuela Politécnica de Cáceres. Universidad de Extremadura.
+// @SolEpcc 
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+// Arduino NodeMCU pin mapping
+#define D0 16
+#define D1 5 // I2C Bus SCL (clock)
+#define D2 4 // I2C Bus SDA (data)
+#define D3 0
+#define D4 2 // Same as "LED_BUILTIN", but inverted logic
+#define D5 14 // SPI Bus SCK (clock)
+#define D6 12 // SPI Bus MISO 
+#define D7 13 // SPI Bus MOSI
+#define D8 15 // SPI Bus SS (CS)
+#define D9 3 // RX0 (Serial console)
+#define D10 1 // TX0 (Serial console)
+
+
+// Timing variables
+const int PROGMEM LED1_change_time = 1000; // interval to change the dimming level
+const int PROGMEM LED2_change_time = 500; // interval to change the dimming level
+unsigned long previousMillis_LED1=0;
+unsigned long previousMillis_LED2=0;
+
+//LED variable
+boolean LED1_state=HIGH;
+boolean LED2_state=HIGH;
+int LED1_pin=D7;
+int LED2_pin=D8;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+  // initialize digital pin LED1 as an output.
+  pinMode(LED1_pin, OUTPUT);
+  // initialize digital pin LED2 as an output.
+  pinMode(LED2_pin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+// Timing the switch of LED1
+  if(millis() - previousMillis_LED1 > LED1_change_time){
+    previousMillis_LED1 = millis(); //update timing variable   
+
+    LED1_state = !LED1_state; //switch LED1 value
+    digitalWrite(LED1_pin, LED1_state);   // write the LED state into the defined pin
+  }
+
+// Timing the switch of LED2
+  if(millis()- previousMillis_LED2 > LED2_change_time){
+    previousMillis_LED2 = millis(); //update timing variable   
+
+    LED2_state = !LED2_state; //switch LED1 value
+    digitalWrite(LED2_pin, LED2_state);   // write the LED state into the defined pin
+  }
+
+
+}
